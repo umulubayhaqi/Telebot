c2c5c2d7b0e8f91cd7f31c54c0d9fa850529e297
umulubayhaqi <76832437+umulubayhaqi@users.noreply.github.com>
2023-05-29 13:38:12 +0700

    Update settings.js
    o

--- settings.js
+++ settings.js
@@ -17,11 +17,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.BOT_TOKEN = "6001993074:AAFKx4HJ6LnqVzo-IioO5bs8U3_1Mod6NmQ" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "UbayBot" //your bot name
+global.OWNER_NAME = "ᴹᴿ᭄ UbayyoFcོ ×፝֟͜×" //your name
+global.OWNER_NUMBER = "6285779172766" //your telegram number
+global.OWNER = ["https://t.me/mynmu", "https://t.me/mynmu"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
